254b26ce9daaa5069575cc4fd1fd63f553c5195a
Mintra Phophaijit <bluembc@gmail.com>
2019-11-13 00:58:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,10 @@ def square_array(numbers)
  new_numbers = []
  numbers.each do |number|
    new_numbers << (number ** 2)
+ end
+ return new_numbers
+ end
+ 
 numbers = [1,2,3]
 numbers.each do |number|
   counter += 1
